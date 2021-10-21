b18a095fde74a86b904bd74307cb0dcde394d93e
dferfer235 <dferfer235@g.educaand.es>
2021-10-21 12:27:22 +0200

    Ej4 repeticion

---
+++ src/main/java/EjEstructuraRepe/Ej04.java
@@ -0,0 +1,23 @@
+package EjEstructuraRepe;
+
+/**
+ * @author davidfdez
+ */
+public class Ej04 {
+
+    public static void main(String[] args) {
+        int numero = 1;
+
+        while (numero < 11) {
+        //El programa comienza realizando un while 
+        //con una serie de numeros del 1 al 10
+            System.out.println("Número: " + numero);
+            numero++;
+        }
+        do {
+        //Despues el programa continua con un dowhile que hace una serie 
+        //que invierte la anterior, desde el 10 hasta el 1
+            System.out.println("Número: " + --numero);
+        } while (numero > 1);
+    }
+}
