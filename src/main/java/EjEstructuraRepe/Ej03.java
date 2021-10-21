8e91cb6a2c09b20183f002ee2dbfeb908f3c62fe
dferfer235 <dferfer235@g.educaand.es>
2021-10-21 12:07:43 +0200

    Ej3 repeticion

---
+++ src/main/java/EjEstructuraRepe/Ej03.java
@@ -0,0 +1,20 @@
+package EjEstructuraRepe;
+
+/**
+ *
+ * @author davidfdez
+ */
+public class Ej03 {
+
+    public static void main(String[] args) {
+        int n = 1, x = 15;
+
+        while (n <= 25) {
+            System.out.print(x);
+            System.out.print(" - ");
+            x = x * 2;
+            n++;
+        }
+
+    }
+}
