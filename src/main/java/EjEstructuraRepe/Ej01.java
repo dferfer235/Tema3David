76ca9ce630416cf8e266e41a3c29d4f122e6042e
dferfer235 <dferfer235@g.educaand.es>
2021-10-21 12:06:58 +0200

    Ej1 repeticion

---
+++ src/main/java/EjEstructuraRepe/Ej01.java
@@ -0,0 +1,18 @@
+package EjEstructuraRepe;
+
+/**
+ * @author davidfdez
+ */
+public class Ej01 {
+
+    public static void main(String[] args) {
+        int x = 2;
+
+        while (x <= 100) {
+            System.out.print(x);
+            System.out.print(" - ");
+            x = x + 1;
+        }
+    }
+        //Serie de nuemros del 2 al 100
+}
