079356b753d30ca9790b95c9d6be4e518b86b9e1
dferfer235 <dferfer235@g.educaand.es>
2021-10-21 12:33:38 +0200

    Retoques ej2 y ej3

--- src/main/java/EjEstructuraRepe/Ej02.java
+++ src/main/java/EjEstructuraRepe/Ej02.java
@@ -11,7 +11,7 @@ public class Ej02 {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         int n, x;
-        System.out.print("Ingrese el valor final:");
+        System.out.println("Ingrese el valor final:");
         n = teclado.nextInt();
         x = 1;
         while (x <= n) {
--- src/main/java/EjEstructuraRepe/Ej03.java
+++ src/main/java/EjEstructuraRepe/Ej03.java
@@ -7,13 +7,13 @@ package EjEstructuraRepe;
 public class Ej03 {
 
     public static void main(String[] args) {
-        int n = 1, x = 15;
+        int contador = 1, x = 15;
 
-        while (n <= 25) {
+        while (contador <= 25) {
             System.out.print(x);
             System.out.print(" - ");
             x = x * 2;
-            n++;
+            contador++;
         }
 
     }
